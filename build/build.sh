d4d0955e91f1c29a709f92c9b72fa49959342682
Mads D. Kristensen <madsdk@gmail.com>
2010-12-09 09:26:42 +0100

    Initial commit.

---
+++ build/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+SRCDIR=../src/
+
+# Check for version number
+if [ "$1" = "" ]; then
+	echo "Usage: build.sh version_number";
+	exit 1
+else
+	VERSION="$1"
+fi
+BUILDDIR=scavenger_daemon-$VERSION
+
+# Create the $BUILDDIR dir
+if [ -d $BUILDDIR ]; then 
+	rm -rf $BUILDDIR;
+fi
+mkdir -p $BUILDDIR
+
+# Copy the needed files into the builddir.
+cp -r $SRCDIR/* $BUILDDIR/
+find $BUILDDIR -name ".git" | xargs rm -rf
+find $BUILDDIR -name ".gitignore" | xargs rm 
+find $BUILDDIR -name ".pyc" | xargs rm
+rm -rf $BUILDDIR/datastore/*
+rm $BUILDDIR/scavenger.ini 
+rm -rf $BUILDDIR/pexecenv/tasks
+
+# Create the archive.
+tar cfvz $BUILDDIR.tar.gz $BUILDDIR
+
+exit 0
